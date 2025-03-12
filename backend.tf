d63ba671ec76fe54453754a7c4a91508683690a9
Henrique Kisaki <satoshikisaki@hotmail.com>
2025-03-11 23:44:14 -0300

    refactor: Rename bucket S3

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eks-fiap-hackathon-terraform-state-bucket"
+    bucket = "fiap-hackathon-terraform-state-bucket"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
